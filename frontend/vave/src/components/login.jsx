24365c872b7f022c844aefe9bb0c33d19871d75d
Jaswanth5996 <jaswanth.140760@gmail.com>
2025-03-01 20:38:54 +0530

    minor-changes

--- frontend/vave/src/components/login.jsx
+++ frontend/vave/src/components/login.jsx
@@ -1,14 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import '../App.css'
-
+// import {} from '@hookform/resolvers/yup'
+// import * as yup from 'yup'
+// import {useForm} from 'react-hook-form'
 
 function Login() {
+    const [number,setNumber]=useState(0)
+    // // const schema = yup.object().shape({
+    // //     telephone : yup.number().max(10).min(10).required("Phone Number can't be empty"),
+    // //     otp: yup.number().integer().min(4).max(4).required("Enter a valid OTP"),
+    // // }
+    // )
     return (
         <div className="container">
             <div className="login-box">
                 <div className="p">Login/register with Phone number</div>
-                <input type="tel" required className="input-box"/>
+                <button>Send OTP</button>
+                <input type="tel" required className="input-box" onChange={(e)=>  {setNumber(e.target.value); console.log(number)}}/>
                 <div className="p">We have sent you a 4-digit OTP</div>
+                {number}
                 <div className="pp">
                     <div></div><div></div><div></div><div></div>
                 </div>
